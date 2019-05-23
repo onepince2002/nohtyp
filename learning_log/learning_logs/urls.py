70f25924f73a7ce5bec51b4fa091baf0d7694bed
eli <eli@yankey.net>
2019-05-23 13:09:16 +0800

    test

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -1,7 +1,19 @@
 from django.conf.urls import url # 匯入url函式，需要用它來映對到URL視窗
+from django.urls import path
 
 from . import views # 匯入views模組，其中句點是讓python從目前的urls.py模組所在的資料夾中匯入視窗
 
-urlpatterns = [
-    url(r'^$', views.index, name='index'), # 此函式接受三個引數來處理，第一個引數是正規表示式，第二個引數指定要呼叫的函式視窗，第三個引數把這個URL模式的名稱指定為index
+
+# urlpatterns = [
+#     url(r'^$', views.index, name='index'), # 此函式接受三個引數來處理，第一個引數是正規表示式，第二個引數指定要呼叫的函式視窗，第三個引數把這個URL模式的名稱指定為index
+# ]
+
+
+app_name='learning_logs'
+urlpatterns=[
+    path('',views.index,name='index'),
+    # 顯示所有主題
+    path('topics/',views.topics,name='topics'),
+    # 顯示個別主題
+    path('<int:topic_id>/',views.topic,name='topic'),
 ]
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
+from .models import Topic # 匯入Topic模型
+
 # Create your views here.
 def index(request):
     return render(request, 'learning_logs/index.html')
+
+
+# 顯示所有主題
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+# 顯示個別主題和它的entries
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
